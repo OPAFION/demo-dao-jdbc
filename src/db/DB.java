0bd4cf54e40bfca8125afb3555b2bfea5f003ffd
Daniel Olivera <daniel13.oliveira.do@gmail.com>
2020-05-28 21:33:11 -0300

    FindById implementation

--- src/db/DB.java
+++ src/db/DB.java
@@ -13,7 +13,7 @@ public class DB {
 
     private static final String DRIVER = "com.mysql.jdbc.Driver";
     private static final String URL = "jdbc:mysql://localhost:3306/coursejdbc";
-    private static final String USER = "developer";
+    private static final String USER = "root";
     private static final String PASS = "Dann847949";
 
     public static Connection getConnection() {
